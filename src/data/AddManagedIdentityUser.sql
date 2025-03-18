e8257dd5986c5759d3434ef5738b26674fd5dc2f
Prakash Beegala <prakash.beegala@microsoft.com>
2025-03-18 14:41:50 +0000

    commit

--- src/data/AddManagedIdentityUser.sql
+++ src/data/AddManagedIdentityUser.sql
@@ -1,4 +1,4 @@
-CREATE USER TechExcelMI FROM EXTERNAL PROVIDER;
-ALTER ROLE db_datareader ADD MEMBER TechExcelMI;
-ALTER ROLE db_datawriter ADD MEMBER TechExcelMI;
+CREATE USER id-g7ybhg6wbdl6i FROM EXTERNAL PROVIDER;
+ALTER ROLE db_datareader ADD MEMBER id-g7ybhg6wbdl6i;
+ALTER ROLE db_datawriter ADD MEMBER id-g7ybhg6wbdl6i;
 
